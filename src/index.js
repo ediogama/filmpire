2290748d77d40d4ea968152377cef4378dbb7758
Edio Gama <ediogama@hotmail.com>
2022-10-17 20:57:22 -0300

    fix error eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
